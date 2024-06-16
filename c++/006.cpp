2dbddbcb2c2ba09b752507536b10a7aab2b522ca
vince0417 <vicenterodrigues564382gmail.com>
2024-06-16 15:12:51 -0300

    Positivo, Negativo ou Nulo

---
+++ c++/006.cpp
@@ -0,0 +1,15 @@
+//Positivo, Negativo ou Nulo
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int num;
+    cin >> num;
+
+    if (num > 0) cout << "positivo" << endl;
+    else if (num == 0) cout << "nulo" << endl;
+    else cout << "negativo" << endl;
+
+    return 0;
+}
