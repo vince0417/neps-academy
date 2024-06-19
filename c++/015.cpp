9ac33c3946e7396e55dccd5f5fd80852c6fa97ed
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2024-06-19 20:44:45 -0300

    Divisão

---
+++ c++/015.cpp
@@ -0,0 +1,14 @@
+//Divisão
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    float n1, n2;
+    cin >> n1 >> n2;
+
+    cout.precision(2);
+    cout.setf(ios::fixed);
+    cout << (n1/n2) << endl;
+    return 0;
+}
