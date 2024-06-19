f65b06ea93e9b509bad6f17b6d416149f9827d2b
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2024-06-19 08:24:26 -0300

    Gangorra

---
+++ c++/012.cpp
@@ -0,0 +1,14 @@
+//Gangorra
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int p1, p2, c1, c2;
+    cin >> p1 >> c1 >> p2 >> c2;
+
+    if ((p1*c1) == (p2*c2)) cout << "0\n";
+    else if ((p1*c1) < (p2*c2)) cout << "1\n";
+    else cout << "-1\n";
+    return 0;
+}
