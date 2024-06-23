68d4b189d544fbf1fe06b0c6b6f4fb14142ea785
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2024-06-23 13:44:30 -0300

    Repetir X Vezes

---
+++ c++/024.cpp
@@ -0,0 +1,16 @@
+//Repetir X Vezes
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int num;
+    cin >> num;
+
+    for (int i {}; i < num; i++)
+    {
+        cout << "NepsAcademy eh Sucesso" << endl;
+    }
+    
+    return 0;
+}
