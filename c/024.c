91dc38d90e749c65402858adb27d833787d74c24
Vicente Rodrigues <vicenterodrigues56438@gmail.com>
2024-06-23 13:46:32 -0300

    Repetir X Vezes

---
+++ c/024.c
@@ -0,0 +1,15 @@
+//Repetir X Vezes
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    scanf("%i", &num);
+
+    for (int i = 0; i < num; i++)
+    {
+        printf("NepsAcademy eh Sucesso\n");
+    }
+    
+    return 0;
+}
